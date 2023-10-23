8bb041f49ce3839b898fb1136cc8ca5142888bb8
jan <xpanj19@gmail.com>
2023-10-23 21:41:11 +0200

    set the size of the cell to 16 characters

--- robotdreams/src/csv_utils.rs
+++ robotdreams/src/csv_utils.rs
@@ -1,19 +1,34 @@
+use csv::{Reader, StringRecord};
 use std::error::Error;
-use csv::Reader;
 
 pub fn process_csv(input: &str) -> Result<(), Box<dyn Error>> {
     let mut reader = Reader::from_reader(input.as_bytes());
 
     let headers = reader.headers()?.clone();
-    let formatted_headers = headers.iter().collect::<Vec<&str>>().join(" | ");
+
+    let formatted_headers = format_csv_row(&headers, 16);
     println!("{}", formatted_headers);
 
     for result in reader.records() {
         let record = result?;
 
-        let formatted_record = record.iter().collect::<Vec<&str>>().join(" | ");
+        let formatted_record = format_csv_row(&record, 16);
         println!("{}", formatted_record);
     }
 
     Ok(())
+}
+
+fn format_csv_row(row: &StringRecord, column_width: usize) -> String {
+    row.iter()
+        .map(|cell| {
+            let truncated = if cell.len() <= column_width {
+                cell.to_string()
+            } else {
+                cell.chars().take(column_width).collect::<String>()
+            };
+            format!("{:<width$}", truncated, width = column_width)
+        })
+        .collect::<Vec<String>>()
+        .join(" | ")
 }
