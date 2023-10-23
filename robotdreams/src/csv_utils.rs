1235843c41d9ecd97ca12b8d84c3566d7b8a58df
jan <xpanj19@gmail.com>
2023-10-23 22:24:42 +0200

    add Csv struct with Display trait

--- robotdreams/src/csv_utils.rs
+++ robotdreams/src/csv_utils.rs
@@ -1,38 +1,15 @@
 use csv::{Reader, StringRecord};
 use std::error::Error;
+use std::fmt;
 
-pub fn process_csv(input: &str) -> Result<(), Box<dyn Error>> {
-    // Create a CSV reader from the input.
-    let mut reader = Reader::from_reader(input.as_bytes());
+struct CsvRecord {
+    data: StringRecord,
+}
 
-    // Read and clone the CSV header.
-    let headers = reader.headers()?.clone();
-
-    // Format and print the CSV header.
-    let formatted_headers = format_csv_row(&headers, 16);
-    println!("{}", formatted_headers);
-
-    for result in reader.records() {
-        let record = match result {
-            Ok(record) => record,
-            Err(err) => {
-                eprintln!("Error reading record: {}", err);
-                continue;
-            }
-        };
-
-        // Check if the number of fields in the record matches the header.
-        if record.len() != headers.len() {
-            eprintln!("Record has a different number of fields. Skipping.");
-            continue; // Skip this record and continue with the next one
-        }
-
-        // Format and print each CSV record.
-        let formatted_record = format_csv_row(&record, 16);
-        println!("{}", formatted_record);
+impl CsvRecord {
+    fn new(data: StringRecord) -> Self {
+        CsvRecord { data }
     }
-
-    Ok(())
 }
 
 // Example of handling more than 16 characters - my approach
@@ -43,17 +20,54 @@ pub fn process_csv(input: &str) -> Result<(), Box<dyn Error>> {
 // Output:
 // name             | surname          | adress
 // Jan              | Pansky           | Tahle adresa ...
-fn format_csv_row(row: &StringRecord, column_width: usize) -> String {
-    row.iter()
-        .map(|cell| {
-            // Truncate cell text if it exceeds 16 characters and add "...".
-            let truncated = if cell.len() <= column_width {
-                cell.to_string()
-            } else {
-                cell.chars().take(column_width - 3).collect::<String>() + "..."
-            };
-            format!("{:<width$}", truncated, width = column_width)
-        })
-        .collect::<Vec<String>>()
-        .join(" | ")
+impl fmt::Display for CsvRecord {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let column_width = 16;
+        // Format each cell in the CSV row, truncating if necessary.
+        let formatted_columns: Vec<String> = self
+            .data
+            .iter()
+            .map(|cell| {
+                let truncated = if cell.len() <= column_width {
+                    cell.to_string()
+                } else {
+                    cell.chars()
+                        .take(column_width - 3)
+                        .collect::<String>()
+                        + "..."
+                };
+                format!("{:<width$}", truncated, width = column_width)
+            })
+            .collect();
+        write!(f, "{}", formatted_columns.join(" | "))
+    }
+}
+
+pub fn process_csv(input: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = Reader::from_reader(input.as_bytes());
+
+    let headers = reader.headers()?.clone();
+
+    let formatted_headers = CsvRecord::new(headers.clone());
+    println!("{}", formatted_headers);
+
+    for result in reader.records() {
+        let record = match result {
+            Ok(record) => CsvRecord::new(record),
+            Err(err) => {
+                eprintln!("Error reading record: {}", err);
+                continue;
+            }
+        };
+
+        // Check if the number of fields in the record matches the header.
+        if record.data.len() != headers.len() {
+            eprintln!("Record has a different number of fields. Skipping.");
+            continue; // Skip this record and continue with the next one
+        }
+
+        println!("{}", record);
+    }
+
+    Ok(())
 }
